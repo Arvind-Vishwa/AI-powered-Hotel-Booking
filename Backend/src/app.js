c857c0a613297eee605d3ad6be0f9ac7be56dd2c
Arvind-Vishwa <av0216380@gmail.com>
2026-04-30 15:11:41 +0530

    changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,13 @@
 const express=require('express')
 const app=express();
+const AuthRouter=require("../src/routes/user.route")
+const cookieParser=require('cookie-parser')
 
 app.use(express.json())
+app.use(cookieParser());
+
+// user route
+app.use('/api/auth',AuthRouter)
 
 
 
