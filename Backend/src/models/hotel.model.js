0b27b4a263cd40fddee3926a7094e321a31f648e
Arvind-Vishwa <av0216380@gmail.com>
2026-05-02 11:11:08 +0530

    Added a room schema

--- Backend/src/models/hotel.model.js
+++ Backend/src/models/hotel.model.js
@@ -1,6 +1,43 @@
 const mongoose=require('mongoose')
 
 
+    const RoomSchema = new mongoose.Schema({
+    hotelId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hotel",
+        required: true
+    },
+
+    type: {
+        type: String,
+        required: true, // e.g. Single, Deluxe, Suite
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+
+    maxGuests: {
+        type: Number,
+        required: true
+    },
+
+    roomNumber: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+
+    unavailableDates: {
+        type: [Date],
+        default: []
+    }
+
+    }, { timestamps: true });
+
+
+
 const hotelSchema=new mongoose.Schema({
     title:{
         type:String,
@@ -24,6 +61,10 @@ const hotelSchema=new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:'user',
         
+    },
+    room:{
+        type:Number,
+        required:true
     }
 })
 
