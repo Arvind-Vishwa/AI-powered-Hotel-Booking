7090d2c6d4ee077016ce99f2f2daf9a14efe3721
Arvind-Vishwa <av0216380@gmail.com>
2026-05-02 11:10:07 +0530

    created a booking model

---
+++ Backend/src/models/book.model.js
@@ -0,0 +1,35 @@
+const mongoose=require('mongoose')
+
+
+const bookingSchema=new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:true
+    },
+    hotelId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"hotel",
+        required:true
+    },
+    checkIn:{
+        type:Date,
+        default:Date.now(),
+        required:true
+    },checkOut:{
+        type:Date,
+        required:true
+    },
+    totalPrice:{
+        type:Number,
+        required:true
+    },
+    selectRoom:{
+        type:Number,
+        required:true
+    }
+})
+
+const bookingModel=mongoose.model('book',bookingSchema)
+
+module.exports=bookingModel
