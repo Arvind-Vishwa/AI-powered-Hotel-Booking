3d042405f4235caace968705eedd3b63fbfa9be3
Arvind-Vishwa <av0216380@gmail.com>
2026-05-02 11:11:49 +0530

    added a booking routes

--- Backend/src/routes/hotel.routes.js
+++ Backend/src/routes/hotel.routes.js
@@ -1,10 +1,12 @@
 const express=require('express')
 const router=express.Router();
 const {createHotelController}=require('../controllers/hotel.controller')
+const {bookingHotelController}=require("../controllers/hotel.controller")
+const authMiddleware=require("../middleware/auth.middleware")
 
 
-router.post('/create',createHotelController)
+router.post('/create',authMiddleware,createHotelController)
 
-
+router.post('/book',authMiddleware,bookingHotelController)
 
 module.exports=router
