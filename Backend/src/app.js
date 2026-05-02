7bd304ec2aaa7c3be3841460e2591cef0b728ea7
Arvind-Vishwa <av0216380@gmail.com>
2026-05-02 09:40:54 +0530

    updated a app.js with hotel router

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,14 +1,20 @@
 const express=require('express')
 const app=express();
-const AuthRouter=require("../src/routes/user.route")
 const cookieParser=require('cookie-parser')
+
+
+// routes
+const AuthRouter=require("../src/routes/user.route")
+const hotelRouter=require("../src/routes/hotel.routes")
+
 
 app.use(express.json())
 app.use(cookieParser());
 
 // user route
-app.use('/api/auth',AuthRouter)
+app.use('/api/auth',AuthRouter);
 
+app.use('/api/hotel',hotelRouter);
 
 
 
