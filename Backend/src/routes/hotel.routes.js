eeff5fbf4444be4b004b545285ccc98cb9c83025
Arvind-Vishwa <av0216380@gmail.com>
2026-05-09 18:47:19 +0530

    updated a hotel routes with multer

--- Backend/src/routes/hotel.routes.js
+++ Backend/src/routes/hotel.routes.js
@@ -3,9 +3,13 @@ const router=express.Router();
 const {createHotelController,getHotelController}=require('../controllers/hotel.controller')
 const {bookingHotelController}=require("../controllers/hotel.controller")
 const authMiddleware=require("../middleware/auth.middleware")
+const {uploadFile}=require('../services/imageKit.service')
 
+const upload=multer({
+    storage:multer.memoryStorage()
+})
 
-router.post('/create',authMiddleware,createHotelController)
+router.post('/create',authMiddleware,upload.single('img'),createHotelController)
 router.get('/getHotel',getHotelController)
 
 
