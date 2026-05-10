5a08c74467b70c77632e2447dc948f96c759b7f6
Arvind-Vishwa <av0216380@gmail.com>
2026-05-10 09:14:17 +0530

    user model updated with role based

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -15,6 +15,11 @@ const userSchema=new mongoose.Schema({
         type:String,
         required:[true,"password is required"],
         // select:false
+    },
+    role:{
+        type:String,
+        enum:["user","owner","admin"],
+        default:"user"
     }
 })
 
