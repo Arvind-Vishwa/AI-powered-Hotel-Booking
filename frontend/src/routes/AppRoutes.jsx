47fcfe7576a0a80062cd623e4328750c86077210
Arvind-Vishwa <av0216380@gmail.com>
2026-05-10 16:32:53 +0530

    added a route of admin owner

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import Dashboard from "../pages/Dashboard";
+
 import CreateHotel from "../pages/CreateHotel";
 import HotelList from "../pages/HotelList";
 import BookHotel from "../pages/BookHotel";
 import { useAuthStore } from "../AuthStore";
+import Owner from "../pages/Dashboard/Owner";
+import User from "../pages/Dashboard/User";
+import Admin from "../pages/Dashboard/Admin";
+import Listing from "../pages/Listing";
 
 const PrivateRoute = ({ children }) => {
   const user = useAuthStore((state) => state.user);
@@ -18,14 +22,14 @@ export default function AppRoutes() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
-      <Route
+      {/* <Route
         path="/"
         element={
           <PrivateRoute>
             <Dashboard />
           </PrivateRoute>
         }
-      />
+      /> */}
 
       <Route
         path="/create"
@@ -39,9 +43,34 @@ export default function AppRoutes() {
       <Route
         path="/hotels"
         element={
-          
             <HotelList />
-          
+        }
+      />
+
+        <Route
+        path="/listing"
+        element={
+            <Listing />
+        }
+      />
+
+      <Route
+        path="/owner"
+        element={
+            <Owner />
+        }
+      />
+
+      <Route
+        path="/user"
+        element={
+            <User />
+        }
+      />
+      <Route
+        path="/admin"
+        element={
+            <Admin />
         }
       />
 
