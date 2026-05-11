21d7729cec6bbe7dfb9944936a79d122a1e09efd
Arvind-Vishwa <av0216380@gmail.com>
2026-05-11 22:17:01 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
-const app=require("./src/app")
-const connectDB=require("./src/config/db")
 
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
 
 connectDB();
 
-app.listen(3000,()=>{
-    console.log("server is listening")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
