1c1bd967a77acc68222f4a027b1b6271ed60dbee
Arvind-Vishwa <av0216380@gmail.com>
2026-05-14 12:29:35 +0530

    update

--- Backend/src/config/gemini.js
+++ Backend/src/config/gemini.js
@@ -1,14 +1,24 @@
-    const {
-        GoogleGenerativeAI,
-    } = require("@google/generative-ai");
-    
-    const genAI =
-        new GoogleGenerativeAI(
-        process.env.GEMINI_API_KEY
-        );
-    
-    const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash",
-    });
-    
-    module.exports = model;
+require("dotenv").config();
+
+const {
+  GoogleGenerativeAI,
+} = require(
+  "@google/generative-ai"
+);
+
+const genAI =
+  new GoogleGenerativeAI(
+    process.env.GEMINI_API_KEY
+  );
+
+const model =
+  genAI.getGenerativeModel({
+
+    model:
+      "gemini-1.5-flash",
+
+  });
+
+  
+
+module.exports = model;
