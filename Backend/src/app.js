cf44d74bc7cd0f8a3672e6494e35859986334634
Arvind-Vishwa <av0216380@gmail.com>
2026-05-14 09:44:32 +0530

    changes in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ const cors=require('cors')
 app.use(cors({
     origin:[
         'http://localhost:5173',
-        'https://boisterous-pika-14800b.netlify.app'
+        'https://singular-cannoli-2a3fd2.netlify.app/'
     ],
     credentials:true
 }))
