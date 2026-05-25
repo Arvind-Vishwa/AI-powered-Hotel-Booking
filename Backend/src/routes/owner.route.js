e1030e42e05a47f4470491817d95543ab1c061c2
Arvind-Vishwa <av0216380@gmail.com>
2026-05-25 08:46:26 +0530

    checking the owner route

--- Backend/src/routes/owner.route.js
+++ Backend/src/routes/owner.route.js
@@ -5,7 +5,7 @@ const {ownerDashboard}=require("../controllers/owner.dashboard.controller")
 const authMiddleware=require("../middleware/auth.middleware")
 
 router.get(
-    "/dashboard",
+    "/owner",
     authMiddleware,
     ownerDashboard
   );
